af27fc290392f6e756f2b83a98563d0a7620b9f1
Rhett Marcelo <rhettmarcelo@outlook.com>
2022-01-21 00:12:35 -0600

    Added error handling

--- allPrimes/allPrimes.cpp
+++ allPrimes/allPrimes.cpp
@@ -32,15 +32,34 @@ void allPrimes(int limit) {
 	}
 	printVector(numbers);
 }
+
+void ignoreLine() { //cleans the buffer
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+int getnumber() {
+	while (true) { //loops when user inputs an invalid value.
+		std::cout << "Please enter a natural number: ";
+		unsigned int number{};
+		std::cin >> number;
+
+		if (std::cin.fail() || number <=0) {
+			std::cin.clear();
+			ignoreLine();
+			std::cerr << "That is an invalid output, please try again.\n";
+		}
+		else {
+			ignoreLine();
+			return number;
+		}
+	}
+
+}
 int main() {
 	std::cout << "This program determines all the prime numbers in the set of natural numbers until the specified N. \n";
-	std::cout << "Please enter a natural number: ";
-
-	int choice{};
-
-	std::cin >> choice;
-
-	allPrimes(choice);
+	int limit { getnumber() };
+	
+	allPrimes(limit);
 
 	return 0;
 }
---
+++ allPrimes/readme.txt
@@ -0,0 +1,2 @@
+1/20/21: created a working program for giving an output of all prime numbers
+1/21/21: added error handling: invalid input. program will loop if user inputs a character, or input results an overflow.
